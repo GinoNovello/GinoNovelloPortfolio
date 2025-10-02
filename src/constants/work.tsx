c6c9d8da30414f0eaa1078dbbb8c1104ad5dacc3
GinoNovello <148281551+GinoNovello@users.noreply.github.com>
2025-10-02 17:50:02 +0000

    update page

--- src/constants/work.tsx
+++ src/constants/work.tsx
@@ -1,45 +1,54 @@
 import { FreelanceLogo } from "@/components/freelance-logo";
 
 interface WorkEntry {
-    id: string;
-    company: string;
-    href: string;
-    badges: string[];
-    location: string;
-    logoUrl: string | React.ReactNode;
-    start: string;
-    end: string | null;
-  }
-
+  id: string;
+  company: string;
+  href: string;
+  badges: string[];
+  location: string;
+  logoUrl: string | React.ReactNode;
+  start: string;
+  end: string | null;
+}
 export const WORK_DATA: readonly WorkEntry[] = [
-    {
-      id: "freelance",
-      company: "Freelance",
-      href: "",
-      badges: [],
-      location: "Remote",
-      logoUrl: <FreelanceLogo/>,
-      start: "May 2023",
-      end: "Present"
-    },
-    {
-      id: "swaply",
-      company: "Swaply",
-      href: "https://swaplyar.vercel.app/",
-      badges: [],
-      location: "Frontend Developer - Remote",
-      logoUrl: "/swaply.webp",
-      start: "Nov 2024",
-      end: "Jan 2025"
-    },
-    {
-      id: "qualita",
-      company: "Qualita",
-      badges: [],
-      href: "https://qualita.studio/",
-      location: "Remote",
-      logoUrl: "/qualita.png",
-      start: "Nov 2023",
-      end: "Oct 2024"
-    },
-  ] as const;
+  {
+    id: "tropware",
+    company: "Tropware",
+    href: "",
+    badges: [],
+    location: "Remote",
+    logoUrl: "/tropware-logo.webp",
+    start: "Feb 2025",
+    end: "Present",
+  },
+  {
+    id: "freelance",
+    company: "Freelance",
+    href: "",
+    badges: [],
+    location: "Remote",
+    logoUrl: <FreelanceLogo />,
+    start: "May 2023",
+    end: "Present",
+  },
+  {
+    id: "swaply",
+    company: "Swaply",
+    href: "https://swaplyar.vercel.app/",
+    badges: [],
+    location: "Frontend Developer - Remote",
+    logoUrl: "/swaply.webp",
+    start: "Nov 2024",
+    end: "Jan 2025",
+  },
+  {
+    id: "qualita",
+    company: "Qualita",
+    badges: [],
+    href: "https://qualita.studio/",
+    location: "Remote",
+    logoUrl: "/qualita.png",
+    start: "Nov 2023",
+    end: "Oct 2024",
+  },
+] as const;
